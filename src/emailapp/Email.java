d5b87aa8c99bf5120174f34f95b5f6fdcbd255e9
naglec13 <naglec13@gmail.com>
2020-02-05 20:57:03 +0300

    Create a constructor setter for the random password

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -8,6 +8,7 @@ public class Email {
     private String password;
     private String department;
     private int mailboxCapacity;
+    private int defaultPasswordLength = 10;
     private String alternateEmail;
 
     // Constructor to receive the first name and last name
@@ -20,11 +21,15 @@ public class Email {
         // Call the method asking the department - return the department
         this.department = setDepartment();
         System.out.println("Department: "+this.department);
+
+        // Call the method that return a random password
+        this.password = randomPassword(defaultPasswordLength);
+        System.out.println("Your password is: " +this.password);
     }
 
 
-    // Ask for the department
 
+    // Ask for the department
     private String setDepartment() {
         System.out.println("DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code: ");
         Scanner in = new Scanner(System.in);
@@ -33,12 +38,18 @@ public class Email {
         if (depChoice == 2) { return "dev"; }
         if (depChoice == 3) { return "acct"; }
         else {return "";}
-
-
     }
 
     // Generate a random password
-
+    private String randomPassword(int length) {
+        String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%";
+        char[] password = new char[length];
+        for (int i = 0; i < password.length; i++) {
+            int rand = (int)(Math.random()*passwordSet.length());
+            password[i]= passwordSet.charAt(rand);
+        }
+        return new String(password);
+    }
     // Set the mailbox capacity
 
     // Set the alternate email
