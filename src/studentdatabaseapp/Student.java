b17431b4267ba5cc39c01bc19c42bb646b206832
naglec13 <naglec13@gmail.com>
2020-02-06 23:17:48 +0300

    Initialize new project StudentDatabaseApp

---
+++ src/studentdatabaseapp/Student.java
@@ -0,0 +1,16 @@
+package studentdatabaseapp;
+
+public class Student {
+
+    // Constructor: prompt user to enter student's name and year
+
+    // Generate an ID
+
+    // Enroll in courses
+
+    // View balance
+
+    // Pay tuition
+
+    //Show status
+}
